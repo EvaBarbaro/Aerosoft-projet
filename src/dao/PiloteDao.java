13396d49d0b72daadd9a5a420fe0b9a4e4b96e8c
EvaBarbaro <barbaro.eva@outlook.fr>
2021-02-10 16:00:42 +0100

    main change

--- src/dao/PiloteDao.java
+++ src/dao/PiloteDao.java
@@ -5,10 +5,47 @@ import java.util.ArrayList;
 
 import connexion.ConnectionBdd;
 import models.*;
+import interfaces.*;
 
-public class PiloteDao {
+public class PiloteDao implements Dao {
 
-	public ArrayList<Pilote> listePilotes() {
+	public PiloteDao() {
+	};
+
+	@Override
+	public Object get(Object id) {
+		Pilote pilote = null;
+
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		String sql = "SELECT * FROM Pilote WHERE IdPilote=?";
+		try {
+			conn = ConnectionBdd.getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setObject(1, id);
+			System.out.println("Voici les informations du vol " + id);
+			ResultSet res = stmt.executeQuery();
+			while (res.next()) {
+
+				pilote = new Pilote(
+					res.getInt("IdPilote"), 
+					res.getString("NomPilote"),
+					res.getString("PrenomPilote"),
+					res.getString("Matricule")
+					);
+			}
+			res.close();
+			conn.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return pilote;
+	}
+
+	@Override
+	public ArrayList<Pilote> getAll() {
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		String sql = "SELECT * FROM pilote";
@@ -22,7 +59,6 @@ public class PiloteDao {
 
 			while (res.next()) {
 
-				//Retrieve by column name
 				Pilote pilote = new Pilote(res.getInt("IdPilote"),res.getString("NomPilote"), res.getString("PrenomPilote"), res.getString("Matricule"));
 
 				listePilotes.add(pilote);
@@ -32,21 +68,22 @@ public class PiloteDao {
 			conn.close();
 			
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 				System.out.println("Impossible d'afficher les vols");
 			}
 		return listePilotes;
 	}
 
-	public static void insertPilote(Pilote pilote) {
+	@Override
+	public void save(Object t) {
+			Pilote pilote = (Pilote) t;
+
 			Connection conn = null;
 			PreparedStatement stmt = null;
 			String sql = "INSERT INTO pilote (NomPilote,PrenomPilote) VALUES (?,?)";
 		try {
 			conn = ConnectionBdd.getConnection();
 			stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-			//Les points d'interogation vont prendre les info de stmt.setString() dans l'ordre par rapport � la requete
 			stmt.setString(1,pilote.getNomPilote());
 			stmt.setString(2,pilote.getPrenomPilote());
 			stmt.execute();
@@ -54,28 +91,61 @@ public class PiloteDao {
 			System.out.println(pilote.getNomPilote()+ " a bien été ajouté");
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.out.println("Impossible d'ajouter un pilote");
 		}
+
 	}
 
-	public static int modifPilote(int id, String newNom, String newPrenom) {
+	@Override
+	public void update(Object t, String[] params) {
+		Pilote pilote = (Pilote) t;
+
 		Connection conn = null;
 		PreparedStatement stmt = null;
 
 		try {
 			conn = ConnectionBdd.getConnection();
 			stmt = conn.prepareStatement("UPDATE pilote SET NomPilote=?,PrenomPilote=? WHERE IdPilote=?");
-			stmt.setString(1, newNom);
-			stmt.setString(2, newPrenom);
-			stmt.setInt(3, id);
-
-			return stmt.executeUpdate();
+			stmt.setString(1, params[0]);
+			stmt.setString(2, params[1]);
+			stmt.setInt(3, pilote.getIdPilote());
+			stmt.executeUpdate();
 		} catch (SQLException e) {
 			// e.printStackTrace();
 			throw new RuntimeException(e);
 		}
+
+	}
+
+	@Override
+	public void delete(Object t) {
+		Pilote pilote = (Pilote) t;
+
+		Connection conn = null;
+		Statement stmt = null;
+		String sql = "DELETE FROM `AVION` WHERE IdPilote=?";
+		try {
+			conn = ConnectionBdd.getConnection();
+			stmt = conn.prepareStatement("DELETE FROM `AVION` WHERE IdPilote=?", Statement.RETURN_GENERATED_KEYS);
+			stmt.setInt(1,pilote.getIdPilote());
+			
+			System.out.println(stmt.toString());
+			
+			stmt.execute();
+			
+			System.out.println(pilote.getMatricule()+ " a bien été Supprimé");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 	
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -8,74 +8,75 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Tapez 1 : Ajouter un nouveau pilote");
-		System.out.println("Tapez 2 : Afficher la liste des vols");
-		System.out.println("Tapez 3 : Rechercher un vol demandé par l'utilisateur");
-		System.out.println("Tapez 4 : Afficher la liste des aéroports par ville");
-		System.out.println("Tapez 5 : Modifier la date d'un vol");
-		System.out.println("Tapez 6 : Supprimer un vol");
-		System.out.println("Tapez 7 : Modifier un pilote");
-		System.out.print("Veuillez entrer votre choix :");
+		System.out.println("Tapez 1 : Test");
+		// System.out.println("Tapez 2 : Afficher la liste des vols");
+		// System.out.println("Tapez 3 : Rechercher un vol demandé par l'utilisateur");
+		// System.out.println("Tapez 4 : Afficher la liste des aéroports par ville");
+		// System.out.println("Tapez 5 : Modifier la date d'un vol");
+		// System.out.println("Tapez 6 : Supprimer un vol");
+		// System.out.println("Tapez 7 : Modifier un pilote");
+		// System.out.print("Veuillez entrer votre choix :");
 		int choix = scanner.nextInt();
 
-		VolDao volDao = new VolDao();
+		// VolDao volDao = new VolDao();
 		
 		Scanner sc = new Scanner(System.in);
 		
 		switch (choix) {
-		// 1 - Add a pilot
+		// // 1 - Add a pilot
 		case 1:
-			Pilote p1 = new Pilote();
-			System.out.println("Ajouter un nouveau Pilote");
-			System.out.println("Entrer le nom : "); 
-			String lastName = sc.nextLine(); 
-			p1.setNomPilote(lastName);
-			System.out.println("Entrer le prenom : "); 
-			String firstName = sc.nextLine(); 
-			p1.setPrenomPilote(firstName);
-			
-			PiloteDao.insertPilote(p1);
-			break;
+		PiloteDao piloteDao = new PiloteDao();
+		// Pilote p1 = new Pilote();
+		System.out.println("Entrez l'id pilote");
+		String idPilote = sc.nextLine();
+		piloteDao.get(Integer.parseInt(idPilote));
+		// 	System.out.println("Entrer le nom : "); 
+		// 	p1.setNomPilote(lastName);
+		// 	System.out.println("Entrer le prenom : "); 
+		// 	String firstName = sc.nextLine(); 
+		// 	p1.setPrenomPilote(firstName);
+		// 	PiloteDao.insertPilote(p1);
+		break;
 
-		case 2:			
-			volDao.listeVols();
-			break;
+		// case 2:			
+		// 	volDao.listeVols();
+		// 	break;
 
-		case 3:
-			System.out.println("Entrer votre numéro de vol: "); 
-			String numVolUser = sc.nextLine();
-			VolDao.rechercheVol(numVolUser);
-			break;
+		// case 3:
+		// 	System.out.println("Entrer votre numéro de vol: "); 
+		// 	String numVolUser = sc.nextLine();
+		// 	VolDao.rechercheVol(numVolUser);
+		// 	break;
 
-		case 4:
-			AeroportDao aeroportDao = new AeroportDao();
-			aeroportDao.listeAeroports();
-			break;
+		// case 4:
+		// 	AeroportDao aeroportDao = new AeroportDao();
+		// 	aeroportDao.getAll();
+		// 	break;
 
-		case 5:
-			System.out.println("Entrer le numéro du vol à modifier"); 
-			String numVol = sc.nextLine(); 
-			System.out.println("Entrer votre nouvelle date :"); 
-			String newDateVol = sc.nextLine(); 
-			AffectationDao.modifVol(numVol, newDateVol);
-			break;
+		// case 5:
+		// 	System.out.println("Entrer le numéro du vol à modifier"); 
+		// 	String numVol = sc.nextLine(); 
+		// 	System.out.println("Entrer votre nouvelle date :"); 
+		// 	String newDateVol = sc.nextLine(); 
+		// 	AffectationDao.modifVol(numVol, newDateVol);
+		// 	break;
 
-		case 6:
-			System.out.println("Entrer le numero de vol à supprimer"); 
-			String numVolDel = sc.nextLine();
-			AffectationDao.delVol(numVolDel);
-			break;
+		// case 6:
+		// 	System.out.println("Entrer le numero de vol à supprimer"); 
+		// 	String numVolDel = sc.nextLine();
+		// 	AffectationDao.delVol(numVolDel);
+		// 	break;
 
-		case 7:
-			System.out.println("Entrer l'id du pilote à modifier");
-			int idPilote = sc.nextInt();
-			sc.nextLine();
-			System.out.println("Entrer le new nom :"); 
-			String newNom = sc.nextLine();
-			System.out.println("Entrer le new prenom"); 
-			String newPrenom = sc.nextLine(); 
-			PiloteDao.modifPilote(idPilote, newNom, newPrenom);
-			break;
+		// case 7:
+		// 	System.out.println("Entrer l'id du pilote à modifier");
+		// 	int idPilote = sc.nextInt();
+		// 	sc.nextLine();
+		// 	System.out.println("Entrer le new nom :"); 
+		// 	String newNom = sc.nextLine();
+		// 	System.out.println("Entrer le new prenom"); 
+		// 	String newPrenom = sc.nextLine(); 
+		// 	PiloteDao.modifPilote(idPilote, newNom, newPrenom);
+		// 	break;
 		}
 	}
 	
