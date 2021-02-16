31f12db5d52905780354a19d9d5dabebfc0b1d7f
EvaBarbaro <barbaro.eva@outlook.fr>
2021-02-16 15:16:02 +0100

    add constraints user menu

--- src/vues/utilisateur/FicheAjoutUtilisateur.java
+++ src/vues/utilisateur/FicheAjoutUtilisateur.java
@@ -72,7 +72,7 @@ public class FicheAjoutUtilisateur extends JFrame implements ActionListener, Key
     public void register() {
         RoleDao roleDao = new RoleDao();
         ArrayList<Role> roles = roleDao.getAll();
-        String roleTab[] = new String[5];
+        String roleTab[] = new String[roles.size()+1];
         roleTab[0] = "Selectionner votre rôle";
         int i = 1;
 
@@ -120,6 +120,7 @@ public class FicheAjoutUtilisateur extends JFrame implements ActionListener, Key
                         JLabel nomPiloteLabel, prenomPiloteLabel, matriculeLabel;
                         JTextField nomPiloteField, prenomPiloteField, matriculeField;
                         JButton btnValidatePilote;
+                        UtilisateurDao utilisateurDao = new UtilisateurDao();
 
                         roleDao.get(stringBox);
 
@@ -135,10 +136,13 @@ public class FicheAjoutUtilisateur extends JFrame implements ActionListener, Key
 
                         nomPiloteLabel = new JLabel("Votre nom");
                         nomPiloteLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+
                         nomPiloteField = new JTextField("");
                         nomPiloteField.setFont(new Font("Tahoma", Font.PLAIN, 15));
+
                         prenomPiloteLabel = new JLabel("Votre prénom");
                         prenomPiloteField = new JTextField("");
+
                         matriculeLabel = new JLabel("Votre matricule");
                         matriculeLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
                         matriculeField = new JTextField("");
@@ -154,29 +158,58 @@ public class FicheAjoutUtilisateur extends JFrame implements ActionListener, Key
                         nomPiloteLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
                         nomPiloteField.setBounds(300, 100, 200, 30);
                         nomPiloteField.setFont(new Font("Tahoma", Font.PLAIN, 15));
+
                         prenomPiloteLabel.setBounds(80, 140, 200, 30);
                         prenomPiloteLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
                         prenomPiloteField.setBounds(300, 140, 200, 30);
                         prenomPiloteField.setFont(new Font("Tahoma", Font.PLAIN, 15));
+
                         matriculeLabel.setBounds(80, 180, 200, 30);
                         matriculeLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
                         matriculeField.setBounds(300, 180, 200, 30);
                         matriculeField.setFont(new Font("Tahoma", Font.PLAIN, 15));
-                        btnInscription.setVisible(false);
 
-                        framePilote.add(nomPiloteLabel);
-                        framePilote.add(nomPiloteField);
-                        framePilote.add(prenomPiloteLabel);
-                        framePilote.add(prenomPiloteField);
-                        framePilote.add(matriculeLabel);
-                        framePilote.add(matriculeField);
-                        framePilote.add(btnValidatePilote);
+                        ArrayList<Utilisateur> userAll = utilisateurDao.getAll();
 
-                        framePilote.setTitle("Aerosoft");
-                        framePilote.setSize(650, 350);
-                        framePilote.setLayout(null);
-                        framePilote.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-                        framePilote.setVisible(true);
+                        Boolean duplicate[] = new Boolean[userAll.size()];
+        
+                        int i = 0;
+                
+                        for (Utilisateur user : userAll) {
+                            if (user.getMail().equals(textFieldLogin.getText())) {
+                                duplicate[i] = true;
+                            }
+                            else {
+                                duplicate[i] = false;
+                            }
+                            i++;
+                        }
+        
+                        if (Arrays.asList(duplicate).contains(true)) {
+                            JLabel labelError = new JLabel("Cette adresse mail existe déjà.");
+                            labelError.setBounds(300, 230, 250, 30);
+                            labelError.setFont(new Font("Tahoma", Font.PLAIN, 15));
+                            labelError.setForeground(Color.RED);
+                            add(labelError);
+                            revalidate();
+                            repaint();
+                        } else {
+                            btnInscription.setVisible(false);
+    
+                            framePilote.add(nomPiloteLabel);
+                            framePilote.add(nomPiloteField);
+                            framePilote.add(prenomPiloteLabel);
+                            framePilote.add(prenomPiloteField);
+                            framePilote.add(matriculeLabel);
+                            framePilote.add(matriculeField);
+                            framePilote.add(btnValidatePilote);
+    
+                            framePilote.setTitle("Aerosoft");
+                            framePilote.setSize(650, 350);
+                            framePilote.setLayout(null);
+                            framePilote.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+                            framePilote.setVisible(true);
+                        }
 
                         roleField = new JTextField(roleAtt.getIdRole());
                         roleField.setVisible(false);
@@ -222,10 +255,32 @@ public class FicheAjoutUtilisateur extends JFrame implements ActionListener, Key
                         add(roleField);
                         break;
                     case "Administrateur":
-                        roleDao.get(stringBox);
-                        roleField = new JTextField(roleAtt.getIdRole());
-                        roleField.setVisible(false);
-                        add(roleField);
+                        UtilisateurDao utilisateurDaoAdmin = new UtilisateurDao();
+                        ArrayList<Utilisateur> userAdmin = utilisateurDaoAdmin.getAll();
+        
+                        int adminCount = 0;
+                
+                        for (Utilisateur users : userAdmin) {
+                            System.out.println(users.getIdRole());
+                            if (users.getIdRole().equals("55555")){
+                                adminCount++;
+                            }
+                        }
+        
+                        if (adminCount == 2) {
+                            JLabel labelErrorAdmin = new JLabel("Le nombre maximal de compte admin a été atteint.");
+                            labelErrorAdmin.setBounds(270, 230, 340, 30);
+                            labelErrorAdmin.setFont(new Font("Tahoma", Font.PLAIN, 15));
+                            labelErrorAdmin.setForeground(Color.RED);
+                            add(labelErrorAdmin);
+                            revalidate();
+                            repaint();
+                        } else {
+                            roleDao.get(stringBox);
+                            roleField = new JTextField(roleAtt.getIdRole());
+                            roleField.setVisible(false);
+                            add(roleField);
+                        }
                         break;
                 }
             }
@@ -240,7 +295,7 @@ public class FicheAjoutUtilisateur extends JFrame implements ActionListener, Key
     }
     
     private void testUtilisateur() {
-        UtilisateurDao utilisateurDao = new UtilisateurDao();
+                UtilisateurDao utilisateurDao = new UtilisateurDao();
                 Utilisateur utilisateur = new Utilisateur();
                 int uniqueID = UUID.randomUUID().hashCode();
 
@@ -248,12 +303,39 @@ public class FicheAjoutUtilisateur extends JFrame implements ActionListener, Key
                 userPiloteString[0] = String.valueOf(uniqueID);
                 userPiloteString[1] = textFieldLogin.getText();
                 userPiloteString[2] = String.valueOf(passwordField.getPassword());
-                userPiloteString[3] = "true";
+                userPiloteString[3] = "false";
                 userPiloteString[4] = roleField.getText();
 
-                utilisateurDao.save(utilisateur, userPiloteString);
+                ArrayList<Utilisateur> userAll = utilisateurDao.getAll();
 
-                dispose();
+                Boolean duplicate[] = new Boolean[userAll.size()];
+
+                int i = 0;
+        
+                for (Utilisateur user : userAll) {
+                    if (user.getMail().equals(userPiloteString[1])) {
+                        duplicate[i] = true;
+                    }
+                    else {
+                        duplicate[i] = false;
+                    }
+                    i++;
+                }
+
+                if (Arrays.asList(duplicate).contains(true)) {
+                    JLabel labelError = new JLabel("Cette adresse mail existe déjà.");
+                    labelError.setBounds(300, 230, 250, 30);
+                    labelError.setFont(new Font("Tahoma", Font.PLAIN, 15));
+                    labelError.setForeground(Color.RED);
+                    add(labelError);
+                    revalidate();
+                    repaint();
+                } else {
+                    utilisateurDao.save(utilisateur, userPiloteString);
+
+                    dispose();
+                }
+
     }
 
     public void actionPerformed(ActionEvent btnEvent) {
