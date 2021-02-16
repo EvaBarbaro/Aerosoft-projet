71454b203e7f61ef7b840c1359a69ca1b57ec967
EvaBarbaro <barbaro.eva@outlook.fr>
2021-02-16 09:52:45 +0100

    user right pilot

--- src/vues/home/MenuMaFiche.java
+++ src/vues/home/MenuMaFiche.java
@@ -29,6 +29,10 @@ public class MenuMaFiche extends JMenu{
 
     private JMenu menuMaFiche;
 
+    PiloteDao piloteDao = new PiloteDao();
+
+    Pilote pilote;
+
     public JMenu getMenu() {
         return this.menuMaFiche;
     }
@@ -120,87 +124,86 @@ public class MenuMaFiche extends JMenu{
     	}
     });
 
-    itemMePilote=new JMenuItem("Mes informations pilotes");
-    itemMePilote.addActionListener(new ActionListener()
-    {
-    	public void actionPerformed(ActionEvent evt)
-    	{
-            JFrame meFramePilote = new JFrame();
-            JLabel labelNom, labelPrenom, labelMatricule;
-            JTextField textFieldId, textFieldNom, textFieldPrenom, textFieldMatricule;
-            JButton btnModifierPilote;
+    pilote = (Pilote) piloteDao.get(idUser);
 
-            PiloteDao piloteDao = new PiloteDao();
-            Pilote pilote;
-            pilote = (Pilote) piloteDao.get(idUser);
-
-            JLabel labelimage = new JLabel();
-            ImageIcon img = new ImageIcon(
-                Toolkit.getDefaultToolkit().getImage(
-                        Fiche.class.getResource("/images/Aerosoft-logo.PNG"))
-                        .getScaledInstance(160, 50, Image.SCALE_DEFAULT)
-                    );
-            labelimage.setBounds(80, 10, 400, 70); 
-            labelimage.setIcon(img);
-            meFramePilote.add(labelimage);
+    if (pilote != null) {
+        itemMePilote=new JMenuItem("Mes informations pilotes");
+        itemMePilote.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent evt)
+            {
+                JFrame meFramePilote = new JFrame();
+                JLabel labelNom, labelPrenom, labelMatricule;
+                JTextField textFieldId, textFieldNom, textFieldPrenom, textFieldMatricule;
+                JButton btnModifierPilote;
     
-            labelNom = new JLabel("Votre Nom:");
-            labelNom.setFont(new Font("Tahoma", Font.PLAIN, 15));
-            labelNom.setBounds(80, 90, 200, 30);
+                JLabel labelimage = new JLabel();
+                ImageIcon img = new ImageIcon(
+                    Toolkit.getDefaultToolkit().getImage(
+                            Fiche.class.getResource("/images/Aerosoft-logo.PNG"))
+                            .getScaledInstance(160, 50, Image.SCALE_DEFAULT)
+                        );
+                labelimage.setBounds(80, 10, 400, 70); 
+                labelimage.setIcon(img);
+                meFramePilote.add(labelimage);
         
-            labelPrenom = new JLabel("Votre Prenom:");
-            labelPrenom.setFont(new Font("Tahoma", Font.PLAIN, 15));
-            labelPrenom.setBounds(80, 130, 200, 30);
-
-            labelMatricule = new JLabel("Votre Matricule:");
-            labelMatricule.setFont(new Font("Tahoma", Font.PLAIN, 15));
-            labelMatricule.setBounds(80, 170, 200, 30);
+                labelNom = new JLabel("Votre Nom:");
+                labelNom.setFont(new Font("Tahoma", Font.PLAIN, 15));
+                labelNom.setBounds(80, 90, 200, 30);
             
-            textFieldId = new JTextField("" + pilote.getIdPilote());
-            textFieldId.setVisible(false);
-        
-            textFieldNom = new JTextField(pilote.getNomPilote());
-            textFieldNom.setFont(new Font("Tahoma", Font.PLAIN, 15));
-            textFieldNom.setBounds(300, 90, 200, 30);
+                labelPrenom = new JLabel("Votre Prenom:");
+                labelPrenom.setFont(new Font("Tahoma", Font.PLAIN, 15));
+                labelPrenom.setBounds(80, 130, 200, 30);
+    
+                labelMatricule = new JLabel("Votre Matricule:");
+                labelMatricule.setFont(new Font("Tahoma", Font.PLAIN, 15));
+                labelMatricule.setBounds(80, 170, 200, 30);
+                
+                textFieldId = new JTextField("" + pilote.getIdPilote());
+                textFieldId.setVisible(false);
             
-            /* Mot de passe*/
-            textFieldPrenom = new JTextField(pilote.getPrenomPilote());
-            textFieldPrenom.setFont(new Font("Tahoma", Font.PLAIN, 15));
-            textFieldPrenom.setBounds(300, 130, 200, 30);
-
-            textFieldMatricule = new JTextField("" + pilote.getMatricule());
-            textFieldMatricule.setFont(new Font("Tahoma", Font.PLAIN, 15));
-            textFieldMatricule.setBounds(300, 170, 200, 30);
-
-            btnModifierPilote = new JButton("Modifier");
-            btnModifierPilote.setBounds(300, 210, 100, 30);
-            btnModifierPilote.setFont(new Font("Tahoma", Font.BOLD, 15));
-            btnModifierPilote.setBackground(new Color(1, 175, 228));
-            btnModifierPilote.setForeground(Color.WHITE);
-            btnModifierPilote.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent eventUpdateUser) 
-                {
-                    String userPilotString[] = new String[5];
-                    userPilotString[0] = textFieldNom.getText();
-                    userPilotString[1] = textFieldPrenom.getText();
-                    userPilotString[2] = textFieldMatricule.getText();
+                textFieldNom = new JTextField(pilote.getNomPilote());
+                textFieldNom.setFont(new Font("Tahoma", Font.PLAIN, 15));
+                textFieldNom.setBounds(300, 90, 200, 30);
+                
+                textFieldPrenom = new JTextField(pilote.getPrenomPilote());
+                textFieldPrenom.setFont(new Font("Tahoma", Font.PLAIN, 15));
+                textFieldPrenom.setBounds(300, 130, 200, 30);
     
-                    piloteDao.update(pilote, userPilotString);
+                textFieldMatricule = new JTextField("" + pilote.getMatricule());
+                textFieldMatricule.setFont(new Font("Tahoma", Font.PLAIN, 15));
+                textFieldMatricule.setBounds(300, 170, 200, 30);
     
-                    meFramePilote.dispose();
-                }
-            });
-
-            meFramePilote.add(labelNom);meFramePilote.add(textFieldNom);meFramePilote.add(labelPrenom);meFramePilote.add(textFieldPrenom);meFramePilote.add(labelMatricule); meFramePilote.add(textFieldMatricule); meFramePilote.add(btnModifierPilote);
-
-            meFramePilote.setTitle("Aerosoft");
+                btnModifierPilote = new JButton("Modifier");
+                btnModifierPilote.setBounds(300, 210, 100, 30);
+                btnModifierPilote.setFont(new Font("Tahoma", Font.BOLD, 15));
+                btnModifierPilote.setBackground(new Color(1, 175, 228));
+                btnModifierPilote.setForeground(Color.WHITE);
+                btnModifierPilote.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent eventUpdateUser) 
+                    {
+                        String userPilotString[] = new String[5];
+                        userPilotString[0] = textFieldNom.getText();
+                        userPilotString[1] = textFieldPrenom.getText();
+                        userPilotString[2] = textFieldMatricule.getText();
         
-            meFramePilote.setSize(650, 350);
-            meFramePilote.setLayout(null);
-            meFramePilote.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-            meFramePilote.setVisible(true);
-    	}
-    });
+                        piloteDao.update(pilote, userPilotString);
+        
+                        meFramePilote.dispose();
+                    }
+                });
+    
+                meFramePilote.add(labelNom);meFramePilote.add(textFieldNom);meFramePilote.add(labelPrenom);meFramePilote.add(textFieldPrenom);meFramePilote.add(labelMatricule); meFramePilote.add(textFieldMatricule); meFramePilote.add(btnModifierPilote);
+    
+                meFramePilote.setTitle("Aerosoft");
+            
+                meFramePilote.setSize(650, 350);
+                meFramePilote.setLayout(null);
+                meFramePilote.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+                meFramePilote.setVisible(true);
+            }
+        });
+    }
 
     menuMaFiche.add(itemMe);
     menuMaFiche.add(itemMePilote);
