5945ecca82a1a9aff9b8991e9a7b957e1bc73f0a
Pierre LANGE <pierre.lange@gmail.com>
2021-02-17 20:19:19 +0100

    Ajout datePicker en mode modif

--- src/dao/RoleDao.java
+++ src/dao/RoleDao.java
@@ -1,194 +1,151 @@
 package dao;
 
-import java.sql.*;
-import java.util.ArrayList;
-
 import connexion.ConnectionBdd;
 import interfaces.Dao;
+import java.sql.*;
+import java.util.ArrayList;
 import models.*;
 import vues.SDialog;
 
 public class RoleDao implements Dao {
-    
-	/** 
-	 * @param id
-	 * @return Object
-	 */
-	public Object get(Object id) {
-		Role role = null;
-        String idSearch = String.valueOf(id);
 
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		String sql = "SELECT * FROM `ROLES` WHERE RoleNom=?";
+  /**
+   * @param id
+   * @return Object
+   */
+  public Object get(Object id) {
+    Role role = null;
+    String idSearch = String.valueOf(id);
 
-		try {
-			conn = ConnectionBdd.getConnection();
-			stmt = conn.prepareStatement(sql);
-			stmt.setString(1, idSearch);
+    Connection conn = null;
+    PreparedStatement stmt = null;
+    String sql = "SELECT * FROM `ROLES` WHERE RoleNom=?";
 
-			System.out.println("Voici les informations du role " + idSearch);
-			ResultSet res = stmt.executeQuery();
+    try {
+      conn = ConnectionBdd.getConnection();
+      stmt = conn.prepareStatement(sql);
+      stmt.setString(1, idSearch);
 
-			while(res.next()){
-				role = new Role(
-					res.getString("IdRole"),
-                    res.getString("RoleNom")
-					);
+      System.out.println("Voici les informations du role " + idSearch);
+      ResultSet res = stmt.executeQuery();
 
-                System.out.println("ID: " + role.getIdRole());
-                System.out.println("Nom: " + role.getRoleNom());
-		      }
-			res.close();
-			conn.close();
+      while (res.next()) {
+        role = new Role(res.getString("IdRole"), res.getString("RoleNom"));
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return role;
-	}
+        System.out.println("ID: " + role.getIdRole());
+        System.out.println("Nom: " + role.getRoleNom());
+      }
+      res.close();
+      conn.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return role;
+  }
 
-    
-	/** 
-	 * @return ArrayList<Role>
-	 */
-	public ArrayList<Role> getAll() {
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		String sql = "SELECT * FROM `ROLES`";
+  /**
+   * @return ArrayList<Role>
+   */
+  public ArrayList<Role> getAll() {
+    Connection conn = null;
+    PreparedStatement stmt = null;
+    String sql = "SELECT * FROM `ROLES`";
 
-		ArrayList<Role> listeRole = new ArrayList<>();
+    ArrayList<Role> listeRole = new ArrayList<>();
 
-		try {
-			conn = ConnectionBdd.getConnection();
-			stmt = conn.prepareStatement(sql);
-			ResultSet res = stmt.executeQuery(sql);
+    try {
+      conn = ConnectionBdd.getConnection();
+      stmt = conn.prepareStatement(sql);
+      ResultSet res = stmt.executeQuery(sql);
 
-			while (res.next()) {
+      while (res.next()) {
+        Role role = new Role(res.getString("IdRole"), res.getString("RoleNom"));
 
-				Role role = new Role(
-					res.getString("IdRole"), 
-					res.getString("RoleNom")
-				);
+        listeRole.add(role);
+      }
 
-				listeRole.add(role);
-			}
-			  
-		    res.close();
-			conn.close();
-			
-			} catch (SQLException e) {
-				e.printStackTrace();
-				System.out.println("Impossible d'afficher les roles");
-			}
-		return listeRole;
-	}
+      res.close();
+      conn.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+      System.out.println("Impossible d'afficher les roles");
+    }
+    return listeRole;
+  }
 
-	
-	/** 
-	 * @param t
-	 * @param params
-	 */
-	@Override
-	public void save(Object t, String[] params) {
-		Role role =(Role) t;
+  /**
+   * @param t
+   * @param params
+   */
+  @Override
+  public void save(Object t, String[] params) {
+    Role role = (Role) t;
 
-		Connection conn = null;
-			PreparedStatement stmt = null;
-			String sql = "INSERT INTO `ROLES` (IdRole,RoleNom) VALUES (?,?)";
+    Connection conn = null;
+    PreparedStatement stmt = null;
+    String sql = "INSERT INTO `ROLES` (IdRole,RoleNom) VALUES (?,?)";
 
-		try {
-			conn = ConnectionBdd.getConnection();
-			stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-			stmt.setString(1,params[0]);
-			stmt.setString(2,params[1]);
-			
-			stmt.execute();
-			
-			System.out.println(role.getIdRole() + " a bien été ajouté");
-			new SDialog("Ajout", "Ajouter reussie", "Valider", "").setVisible(true);
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("Impossible d'ajouter un pilote");
-			new SDialog("Echec", "L'ajout n'a pas reussie car " + e, "ok", "").setVisible(true);
-			throw new RuntimeException(e);
-		}
+    try {
+      conn = ConnectionBdd.getConnection();
+      stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+      stmt.setString(1, params[0]);
+      stmt.setString(2, params[1]);
 
-	}
+      stmt.execute();
 
-	
-	/** 
-	 * @param t
-	 * @param params
-	 */
-	@Override
-	public void update(Object t, String[] params) {
-		/*Role role =(Role) t;
+      System.out.println(role.getIdRole() + " a bien été ajouté");
+      new SDialog("Ajout", "Ajouter reussie", "Valider", "").setVisible(true);
+    } catch (SQLException e) {
+      e.printStackTrace();
+      System.out.println("Impossible d'ajouter un pilote");
+      new SDialog("Echec", "L'ajout n'a pas reussie car " + e, "ok", "")
+      .setVisible(true);
+      throw new RuntimeException(e);
+    }
+  }
 
-		Connection conn = null;
-			PreparedStatement stmt = null;
-			String sql = "UPDATE `ROLES` SET IdRole='" + params[0] + "',RoleNom='" + params[1] + "' WHERE RoleNom LIKE '" + params[1] + "'";
+  /**
+   * @param t
+   * @param params
+   */
+  @Override
+  public void update(Object t, String[] params) {}
 
-		try {
-			conn = ConnectionBdd.getConnection();
-			stmt = conn.prepareStatement(sql);
+  /**
+   * @param t
+   */
+  @Override
+  public void delete(Object t) {
+    // TODO Auto-generated method stub
 
-			System.out.println("update role stmt :" + stmt);
-			stmt.executeUpdate(sql);
-			
-			System.out.println(role.getIdRole() + " a bien été ajouté");
-			new SDialog("update", "update reussie", "Valider", "").setVisible(true);
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-			System.out.println("Impossible d'ajouter un pilote");
-			new SDialog("Echec", "L'update n'a pas reussie car " + e, "ok", "").setVisible(true);
-			throw new RuntimeException(e);
-		}
-*/
-	}
+  }
 
-	
-	/** 
-	 * @param t
-	 */
-	@Override
-	public void delete(Object t) {
-		// TODO Auto-generated method stub
+  /**
+   * @param idSearch
+   * @return String
+   */
+  public String getRoleNomByIdRole(String idSearch) {
+    String nom = "";
+    Connection conn = null;
+    PreparedStatement stmt = null;
+    String sql = "SELECT RoleNom FROM `ROLES` WHERE IdRole=?";
 
-	}
+    try {
+      conn = ConnectionBdd.getConnection();
+      stmt = conn.prepareStatement(sql);
+      stmt.setString(1, idSearch);
 
-	
-	/** 
-	 * @param idSearch
-	 * @return String
-	 */
-	public String getRoleNomByIdRole(String idSearch) {
-		
-		String nom = ""
-		;		
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		String sql = "SELECT RoleNom FROM `ROLES` WHERE IdRole=?";
+      System.out.println("Voici les informations du role " + idSearch);
+      ResultSet res = stmt.executeQuery();
 
-		try {
-			conn = ConnectionBdd.getConnection();
-			stmt = conn.prepareStatement(sql);
-			stmt.setString(1, idSearch);
-
-			System.out.println("Voici les informations du role " + idSearch);
-			ResultSet res = stmt.executeQuery();
-
-			while(res.next()){
-				nom = res.getString("RoleNom");
-		      }
-			res.close();
-			conn.close();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return nom;
-	}
+      while (res.next()) {
+        nom = res.getString("RoleNom");
+      }
+      res.close();
+      conn.close();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return nom;
+  }
 }
